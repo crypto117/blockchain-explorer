775df7bf4a0690e8bd4ce1339cba5c7c2f43c28f
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-02-24 16:01:56 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const ExplorerConfig = {
-  apiBaseUrl: 'https://68.183.158.87:8004',
+  apiBaseUrl: 'http://104.248.211.207/',
   poolApiUrl: 'https://rhox.cryptonote.club:8199/stats',
   poolListUrl: 'https://raw.githubusercontent.com/turtlecoin/turtlecoin-pools-json/master/v2/turtlecoin-pools.json',
   ticker: 'RHOX',
