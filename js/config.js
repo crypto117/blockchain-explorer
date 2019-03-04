396a9885d6fbe5d5f6a4bf464d9fccf41cda7eea
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-03-04 02:20:08 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const ExplorerConfig = {
   poolListUrl: 'https://raw.githubusercontent.com/turtlecoin/turtlecoin-pools-json/master/v2/turtlecoin-pools.json',
   ticker: 'RHOX',
   maxSupply: 2100000000000000000,
-  decimalPoints: 2,
+  decimalPoints: 8,
   blockTargetTime: 120,
   addressPrefix: 0x14d893,
   forkHeights: [
